20cceb8e4eb92714f56bb79f9af2f44d11b36049
Vasko Mihaylov <vasko.mihaylov@publicisgroupe.com>
2023-08-15 17:34:29 +0300

    Setup from https://github.com/nextcloud/docker/tree/master/.examples/docker-compose

--- set_configuration.sh
+++ set_configuration.sh
@@ -2,15 +2,6 @@
 
 set -x
 
-docker exec -u www-data app-server php occ --no-warnings config:system:get trusted_domains >> trusted_domain.tmp
-
-if ! grep -q "nginx-server" trusted_domain.tmp; then
-    TRUSTED_INDEX=$(cat trusted_domain.tmp | wc -l);
-    docker exec -u www-data app-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="nginx-server"
-fi
-
-rm trusted_domain.tmp
-
 docker exec -u www-data app-server php occ --no-warnings app:install onlyoffice
 
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
